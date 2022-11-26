eaa37b17b3d4a1c62bc0160a26fffb0f995d35ea
Nicolas Sutter <contact.sutter.nicolas@gmail.com>
2022-11-26 11:05:03 +0100

    feat: add utilities

---
+++ src/utils.ts
@@ -0,0 +1,13 @@
+import { isPackageExists as packageExists } from 'local-pkg'
+
+export function removeUnusedItems (items: string[]) {
+  return items.filter((item) => item !== '')
+}
+
+export const TS = packageExists('typescript')
+export const Nuxt = packageExists('nuxt')
+export const Vue = packageExists('vue')
+export const Prettier = packageExists('prettier')
+export const EslintPluginPrettier = packageExists('eslint-plugin-prettier')
+export const VueEslintTypescript = packageExists('@vue/eslint-config-typescript')
+export const VueEslintPrettier = packageExists('@vue/eslint-config-prettier')

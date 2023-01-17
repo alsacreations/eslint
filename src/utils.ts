40b3750cf7e1efb7df21930b09008d177eb46344
Nicolas Sutter <contact.sutter.nicolas@gmail.com>
2023-01-17 16:40:09 +0100

    feat: add `isPackageListed`

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,29 @@
-import { isPackageExists as packageExists } from 'local-pkg'
+import { isPackageExists as packageExists, PackageInfo } from 'local-pkg'
+import { readFileSync } from 'node:fs'
+import { resolve } from 'node:path'
+
+/**
+ * Permet de savoir si un certain package est listé
+ * dans le package.json du process en cours.
+ *
+ * @param name Le nom du package
+ */
+export const isPackageListed = (name: string) => {
+  const packageJson: Partial<PackageInfo['packageJson']> = (() => {
+    try {
+      return JSON.parse(readFileSync(
+        resolve(process.cwd(), 'package.json')
+      ).toString())
+    } catch (error) {
+      return {}
+    }
+  })()
+
+  const deps = packageJson?.dependencies ?? {}
+  const devDeps = packageJson?.devDependencies ?? {}
+
+  return name in deps || name in devDeps
+}
 
 export function removeUnusedItems (items: string[]) {
   return items.filter((item) => item !== '')

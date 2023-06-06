5f4d2272875a80db5f89c6c666f13c748f48043a
Nicolas Sutter <contact.sutter.nicolas@gmail.com>
2023-06-06 09:10:21 +0200

    refactor: use vue eslint config prettier with the `skip-formatting` config

--- src/prettier/index.ts
+++ src/prettier/index.ts
@@ -5,7 +5,7 @@ import { removeUnusedItems } from '../utils'
 export = defineConfig({
   extends: removeUnusedItems([
     // Gestion de prettier quand vue est présent
-    Prettier && Vue && VueEslintPrettier ? '@vue/eslint-config-prettier' : '',
+    Prettier && Vue && VueEslintPrettier ? '@vue/eslint-config-prettier/skip-formatting' : '',
     // Gestion de prettier sans vue (ou nuxt), si vue est présent, on utilise le plugin @vue
     Prettier && !Vue && EslintConfigPrettier ? 'prettier' : '',
   ]),
